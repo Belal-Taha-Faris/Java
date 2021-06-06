b238e8aa757ae71ea277cd9faef8135c9980a945
Belal Taha <bt2970918@gmail.com>
2021-06-06 07:29:33 +0200

    print your name

---
+++ Java-Print-your-name/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String fname = "Belal" , lname = "Taha";
+        System.out.println(fname + " " + lname);
+    }
+}
