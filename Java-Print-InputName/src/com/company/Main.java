2e3b9dd356fd84cbdee1ce7d2dd8cef0be81fd6e
Belal Taha <bt2970918@gmail.com>
2021-06-08 11:27:31 +0200

    print input name
    take an input as string from user and print welcome msg by his name

---
+++ Java-Print-InputName/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner x   = new Scanner(System.in);
+
+        System.out.println("Enter your name pls");
+        String name = x.nextLine();
+
+        System.out.println("Welcome "+ name);
+    }
+}
